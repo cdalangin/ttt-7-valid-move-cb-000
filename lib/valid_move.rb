4d819a45eed7f41d91811d36a9da04cdb0acde0f
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-17 20:24:24 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between?(1,9) == true && (position_taken?(board, index) == false)   #if position is within 1=9
+  if index.between?(0,8) == true && (position_taken?(board, index) == false)   #if position is within 1=9
       return true
   else
       return false
