079ef144d8094c64c1185c261646321f86d15acb
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-17 20:14:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if index.between?(1,9) == true   #if position is within 1=9
     if position_taken?(board, index) == false #BUT its taken
       return true
-    else 
+    else
       return false
     end
 end
