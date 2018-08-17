b904bd7c1670a6d30a5768eaea799eaf24e1690b
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-17 19:44:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if index.between?(1,9)    #if position is within 1=9
-    if position_taken?(board, index)   #BUT its taken
-      return false
-    else
+    if position_taken?(board, index) == false #BUT its taken
       return true
+    else
+      return false
     end
   elsif index <=0 || index >= 10   #if it is NOT within 1-9
     return false
